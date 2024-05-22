fb1db135c0919adf93c58ab3f0f9b3718dff4eaf
Ricardo Sueiras <ricardo.sueiras@gmail.com>
2024-05-22 19:34:00 +0200

    Add context and resource examples

--- cedar-flask-demo/flask-demo/app.py
+++ cedar-flask-demo/flask-demo/app.py
@@ -84,9 +84,9 @@ def login():
             global request_manage
             global public_photos
             request_adm = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"siteAdmin\"","resource": "PhotoApp::Photo::\"\"","context": { }}
-            request_photo = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"viewPhoto\"","resource": "PhotoApp::Album::\"DoePhotos\"","context": { }}
+            request_photo = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"viewPhoto\"","resource": "PhotoApp::Album::\"DoePhotos\"", "context": { }}
             public_photos = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"viewPhoto\"","resource": "PhotoApp::Album::\"DoePublicPhotos\"","context": { }}
-            request_manage = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"managePhoto\"","resource": "PhotoApp::Album::\"DoePhotos\"","context": { }}
+            request_manage = {"principal": f"PhotoApp::User::\"{current_user.id}\"","action": "PhotoApp::Action::\"managePhoto\"","resource": "PhotoApp::Album::\"DoePhotos\"","context": { "MFAEnable" : True }}
             return redirect(url_for('index'))
         else:
             flash('Invalid credentials', 'danger')
